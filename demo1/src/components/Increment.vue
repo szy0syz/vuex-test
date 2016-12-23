e1af376cad295c73c47acb3104ea62e5c7e1c864
szy0syz <szy0syz@gmail.com>
2016-12-23 23:36:20 +0800

    step02: create action

--- demo1/src/components/Increment.vue
+++ demo1/src/components/Increment.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <button>Increment +1</button>
+    <button @click='incremnet'>Increment +1</button>
   </div>
 </template>
 
 <script>
+import { incrementCounter } from '../vuex/actions'
 export default {
+  vuex: {
+    actions: {
+      increment: incrementCounter
+    }
+  }
 }
 </script>
