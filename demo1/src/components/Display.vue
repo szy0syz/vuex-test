252285f9aae9b378c62281a984fc504d51da3a95
szy0syz <szy0syz@gmail.com>
2016-12-23 23:40:42 +0800

    step04: getter

--- demo1/src/components/Display.vue
+++ demo1/src/components/Display.vue
@@ -5,6 +5,13 @@
 </template>
 
 <script>
+import { getCount } from '../vuex/getters'
 export default {
+  vuex: {
+    getters: {
+      // 注意在这里你需要 `getCount` 函数本身而不是它的执行结果 'getCount()'
+      counterValue: getCount
+    }
+  }
 }
 </script>
