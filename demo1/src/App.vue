29b5db4464acb606d123f945700db7bc0434de1a
szy0syz <szy0syz@gmail.com>
2016-12-23 15:34:38 +0800

    init OK

---
+++ demo1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Display></Display>
+    <Increment></Increment>
+  </div>
+</template>
+
+<script>
+import Display from './components/Display.vue'
+import Increment from './components/Increment.vue'
+
+export default {
+  components: {
+    Display: Display,
+    Increment: Increment
+  }
+}
+</script>
