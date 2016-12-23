80038ae0fac6ea64dc735a2fd19741ad425c6f21
szy0syz <szy0syz@gmail.com>
2016-12-23 15:55:07 +0800

    step01: add state

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -8,11 +8,13 @@
 <script>
 import Display from './components/Display.vue'
 import Increment from './components/Increment.vue'
+import store from './vuex/store'
 
 export default {
   components: {
     Display: Display,
-    Increment: Increment
-  }
+    Increment: Increment,
+  },
+  store: store // 在根组件加入 store，让它的子组件和 store 链接
 }
 </script>
---
+++ demo1/src/vuex/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// 创建一个对象来保存应用启动时的初始状态
+const state = {
+  // TODO: 放置初始状态
+}
+
+// 创建一个对象
+const mutations = {
+  // TODO: 放置我们的状态变更函数
+}
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
