c7a4c1f88f825e2b84f6b123379761df6d36c408
szy0syz <szy0syz@gmail.com>
2016-12-23 23:38:14 +0800

    create state & mutation

--- demo1/src/vuex/store.js
+++ demo1/src/vuex/store.js
@@ -6,11 +6,17 @@ Vue.use(Vuex)
 // 创建一个对象来保存应用启动时的初始状态
 const state = {
   // TODO: 放置初始状态
+  count: 0
 }
 
 // 创建一个对象
 const mutations = {
   // TODO: 放置我们的状态变更函数
+  // mutation 的第一个参数是当前的 state
+  // 你可以在函数里修改 state
+  INCREMENT (state, amount) {
+  	state.count = state.count + amount
+  }
 }
 
 export default new Vuex.Store({
